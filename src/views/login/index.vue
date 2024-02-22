abbe88f15813a5853fffeb88587519a20be98e08
brokyz <79079781+brokyz@users.noreply.github.com>
2024-02-22 16:26:40 +0000

    登录表单验证

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,41 +2,24 @@
   <div class="login-container">
     <div class="login-image"></div>
     <el-card class="login-card" shadow="always">
-      <el-form
-        class="login-form"
-        autocomplete="false"
-        :model="loginForm"
-        :rules="loginRules"
-      >
+      <el-form class="login-form" ref="form" autocomplete="false" :model="loginForm" :rules="loginRules">
         <el-form-item>
           <h2>登录</h2>
         </el-form-item>
-        <el-form-item prop="mobile">
-          <el-input
-            placeholder="请输入用户名"
-            type="text"
-            prefix-icon="el-icon-user"
-            v-model="loginForm.username"
-          ></el-input>
+        <el-form-item prop="username">
+          <el-input placeholder="请输入用户名" type="text" prefix-icon="el-icon-user" v-model="loginForm.username"></el-input>
         </el-form-item>
         <el-form-item prop="password">
-          <el-input
-            placeholder="请输入密码"
-            type="password"
-            prefix-icon="el-icon-lock"
-            v-model="loginForm.password"
-          ></el-input>
+          <el-input placeholder="请输入密码" type="password" prefix-icon="el-icon-lock"
+            v-model="loginForm.password"></el-input>
+        </el-form-item>
+        <el-form-item prop="isAgree">
+          <el-checkbox v-model="loginForm.isAgree">我已阅读并同意《用户协议》</el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-checkbox v-model="loginForm.isAgree"
-            >我已阅读并同意《用户协议》</el-checkbox
-          >
+          <el-button type="primary" style="width: 100%" @click="login">登录</el-button>
         </el-form-item>
-        <el-form-item>
-          <el-button type="primary" style="width: 100%">登录</el-button>
-        </el-form-item>
-      </el-form></el-card
-    >
+      </el-form></el-card>
   </div>
 </template>
 
@@ -45,13 +28,15 @@ export default {
   name: "login",
   data() {
     return {
+      // 登录表单的数据
       loginForm: {
         username: "",
         password: "",
         isAgree: "",
       },
+      // 登录表单的验证
       loginRules: {
-        mobile: [
+        username: [
           {
             required: "true",
             message: "请输入用户名",
@@ -70,10 +55,22 @@ export default {
             message: "密码长度应位于6-16位之间",
           },
         ],
-        isAgree: [],
+        isAgree: [{
+          validator: (rule, value, callback) => {
+
+            value ? callback() : callback(new Error('请阅读并勾选用户协议'))
+          }
+        }],
       },
     };
   },
+  methods: {
+    login() {
+      this.$ref.form.validator((isOK) => {
+        isOK ? alert(123) : ""
+      })
+    }
+  }
 };
 </script>
 
@@ -82,6 +79,7 @@ export default {
   height: 100vh;
   display: flex;
   background: linear-gradient(to bottom, #f8f9fa, #e9f2fe);
+
   .login-image {
     width: 60%;
     background-color: pink;
